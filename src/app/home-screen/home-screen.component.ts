9cfc16a6bffc066a3e8ab61adbfbe1261be78892
Regiany Almeida <regiany.nunes@ibm.com>
2020-03-18 19:27:19 -0300

    adicionando roteamento para tela tela de detalhes

--- src/app/home-screen/home-screen.component.ts
+++ src/app/home-screen/home-screen.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { DomSanitizer } from '@angular/platform-browser';
 
 import { ProductService } from '../../services/products/product.service'
 import { AuthService } from '../../services/authentication/auth.service';
@@ -14,8 +15,9 @@ export class HomeScreenComponent implements OnInit {
   products: any
   oneProduct: any;
   profile: any
+  imagePath: any;
 
-  constructor(private productService: ProductService, private router: Router, private authService: AuthService) { }
+  constructor(private productService: ProductService, private router: Router, private authService: AuthService, private _sanitizer: DomSanitizer ) { }
 
   ngOnInit() {
     this.getProducts()
@@ -23,30 +25,32 @@ export class HomeScreenComponent implements OnInit {
   }
 
   getProducts(){
-
     return this.productService.getAllProducts().subscribe(response =>{
       console.log("Resposta", response.data)
       
       this.products = response.data
-
     })
   }
 
   getActiveProducts(){
-
     return this.productService.getAllActiveProducts().subscribe(response =>{
       console.log("Resposta", response.data)
       
       this.products = response.data
-
     })
+  }
+
+  getImagePath(product){
+    let link = 'https://lojinha.s3.us-south.cloud-object-storage.appdomain.cloud/' + product.imageName + '.jpg' 
+    this.imagePath = this._sanitizer.bypassSecurityTrustUrl(link)
   }
 
   openProductDetails(product){
     console.log('---openProductDetails---',product)
     this.oneProduct = product
-    this.router.navigate(['/gerenciar/produtos'], {state: {data: product}});
-
+    this.getImagePath(this.oneProduct)
+    this.oneProduct['imagePath'] = this.imagePath.changingThisBreaksApplicationSecurity
+    this.router.navigate(['/gerenciar/produtos'], {state: {data: this.oneProduct}});
   }
   
   inactiveProduct(product) {
